83a8e4dd84ca2b57fc10db27e9b460b0b0e71903
AbdulSamad <asmadni45@gmail.com>
2025-02-25 16:35:32 +0500

    Update library version

--- smartswitch/build.gradle.kts
+++ smartswitch/build.gradle.kts
@@ -46,7 +46,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.imranshafique" // Your GitHub username
                 artifactId = "cybronsmartswitchsdk"
-                version = "1.0"
+                version = "1.1"
             }
         }
     }
