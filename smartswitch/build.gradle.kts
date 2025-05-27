ae222a90829a22d04eaa59f8b14c9bd69f06b53d
Ameer Mavia <ameermaviacyb@gmail.com>
2025-05-27 11:45:32 +0500

    VERsion updated

--- smartswitch/build.gradle.kts
+++ smartswitch/build.gradle.kts
@@ -46,7 +46,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.imranshafique" // Your GitHub username
                 artifactId = "cybronsmartswitchsdk"
-                version = "1.2"
+                version = "1.3"
             }
         }
     }
