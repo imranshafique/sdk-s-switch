82013c9b2f5cbcebd3b850e35df570b0f8cef610
imranshafique <raja.imran@gmail.com>
2026-09-10 06:52:29 +0800

    Configure release publication for AGP 9

--- smartswitch/build.gradle.kts
+++ smartswitch/build.gradle.kts
@@ -28,6 +28,12 @@ android {
         buildConfig=true
     }
 
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
