8fe37a532fbbb263f38afa09d46201d158b3a5fd
imranshafique <raja.imran@gmail.com>
2026-09-10 06:45:34 +0800

    Modernize Android build and add remediation plan

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    id ("kotlin-kapt")
-    id ("dagger.hilt.android.plugin")
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -33,10 +32,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     buildFeatures {
         viewBinding = true
         buildConfig = true
@@ -50,37 +45,18 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
 
-    // Hilt dependencies
-    implementation ("com.google.dagger:hilt-android:2.48.1")
+    implementation(libs.hilt.android)
     implementation(project(":smartswitch"))
-    kapt ("com.google.dagger:hilt-android-compiler:2.48.1")
-
-    // Optional: Hilt ViewModel and Navigation support
-//    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.2")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.7.2")
+    ksp("com.google.dagger:hilt-compiler:2.60.1")
 
     // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    // ViewPager2 for swiping between fragments
-    implementation("androidx.viewpager2:viewpager2:1.0.0")
-
-    // Data Binding (ViewBinding)
-    implementation("androidx.databinding:viewbinding:7.0.4")
-
-    // MVVM Architecture Components
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
-
-    // For DocumentsManager and other storage access
-    implementation("androidx.documentfile:documentfile:1.0.1")
-
-    // Shapeable
-    implementation("com.google.android.material:material:1.2.0")
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.11.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.11.0")
 
     // SDP and SSP
     implementation("com.intuit.sdp:sdp-android:1.1.1")
@@ -91,46 +67,11 @@ dependencies {
 
     // Glide
     implementation("com.github.bumptech.glide:glide:4.15.1")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
-
-    // Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
-
-    implementation ("com.hbb20:ccp:2.5.0")
-    implementation ("androidx.room:room-runtime:2.5.0")
-    kapt ("androidx.room:room-compiler:2.5.0")
-    implementation ("androidx.room:room-ktx:2.5.0")
-    implementation ("com.google.code.gson:gson:2.8.9")
-    implementation (platform("com.google.firebase:firebase-bom:31.0.1"))
-    implementation ("com.google.firebase:firebase-analytics")
-//    implementation 'com.google.firebase:firebase-config'
-    implementation ("com.google.firebase:firebase-config-ktx")
-    implementation ("com.google.firebase:firebase-crashlytics-buildtools:2.9.2")
     implementation ("com.airbnb.android:lottie:5.2.0")
-    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
-    implementation("com.google.zxing:core:3.5.1")
-
-
-    // camera x
-    val cameraxVersion = "1.5.0-alpha05"
-    // The following line is optional, as the core library is included indirectly by camera-camera2
-    implementation("androidx.camera:camera-core:${cameraxVersion}")
+    val cameraxVersion = "1.6.2"
     implementation("androidx.camera:camera-camera2:${cameraxVersion}")
-    // If you want to additionally use the CameraX Lifecycle library
     implementation("androidx.camera:camera-lifecycle:${cameraxVersion}")
-    // If you want to additionally use the CameraX View class
     implementation("androidx.camera:camera-view:${cameraxVersion}")
-    // If you want to additionally use the CameraX Extensions library
-    implementation("androidx.camera:camera-extensions:${cameraxVersion}")
-
-    // ML kit barcode reader
     implementation("com.google.mlkit:barcode-scanning:17.3.0")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-
-}
-kapt {
-    correctErrorTypes =true
 }
 
--- smartswitch/build.gradle.kts
+++ smartswitch/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
     id("maven-publish")
 }
 
@@ -34,9 +33,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 }
 // Publishing configuration
 afterEvaluate {
@@ -55,11 +51,7 @@ afterEvaluate {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.hilt.android)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
