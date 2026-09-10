4097f9ac5cc893e4f0b5e3eaed388c5585f0ad91
imranshafique <raja.imran@gmail.com>
2026-09-10 22:03:18 +0800

    Enable AGP R8 release optimization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,11 +20,9 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            optimization {
+                enable = true
+            }
         }
     }
 
