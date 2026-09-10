bd32af696008e741d6b4cd95be6c70321ed335d9
imranshafique <raja.imran@gmail.com>
2026-09-10 18:00:41 +0800

    Restore required Glide Hilt binding

---
+++ app/src/main/java/com/msn/dataselectionviewpager/Utils/GlideModule.kt
@@ -0,0 +1,20 @@
+package com.msn.dataselectionviewpager.Utils
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object GlideModule {
+    @Provides
+    @Singleton
+    fun provideRequestManager(@ApplicationContext context: Context): RequestManager =
+        Glide.with(context)
+}
